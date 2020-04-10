3a1a2c4c1aef6bf1824c5b88dba6484304932a18
Michael Terry <mike.a.terry@gmail.com>
2020-04-10 16:29:28 -0400

    Fixing tfa_error for build

--- sound/soc/codecs/tfa98xx.c
+++ sound/soc/codecs/tfa98xx.c
@@ -2291,7 +2291,7 @@ static void tfa98xx_container_loaded(const struct firmware *cont, void *context)
 {
 	nxpTfaContainer_t *container;
 	struct tfa98xx *tfa98xx = context;
-	enum Tfa98xx_Error tfa_err;
+	enum tfa_error tfa_err;
 	int container_size;
 	int ret;
 
@@ -2326,8 +2326,8 @@ static void tfa98xx_container_loaded(const struct firmware *cont, void *context)
 		pr_debug("%d nprof\n", container->nprof);
 
 		tfa_err = tfa_load_cnt(container, container_size);
-		if (tfa_err != Tfa98xx_Error_Ok) {
-			mutex_unlock(&tfa98xx_mutex);
+		if (tfa_err != tfa_error_ok) {
+      mutex_unlock(&tfa98xx_mutex);
 			kfree(container);
 			dev_err(tfa98xx->dev, "Cannot load container file, aborting\n");
 			return;
